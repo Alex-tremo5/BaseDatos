27fb810fa8528c97674af3b39511bd84fcff1a35
Chokis <mapaulaquintero@gmail.com>
2024-04-03 23:07:58 -0500

    form stylish

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,34 @@
 import React from 'react'
 
 function FormCollaborator(props) {
+
     return (
-        <form>
+        <>
+        <form className='registro'>
+            <h2>Add collaborator</h2>
             <div className="mb-3">
-                <label for="inputName" className="form-label">Name</label>
-                <input type="text" className="form-control" id="inputName"/>
+                
+                <input type="text" className="form-control" id="inputName" placeholder='Name'/>
             </div>
             <div className="mb-3">
-                <label for="inputEmail" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="inputEmail"/>
+                <input type="email" className="form-control" id="inputEmail" placeholder='Email'/>
             </div>
             <div className="mb-3">
-                <label for="inputAge" className="form-label">Age</label>
-                <input type="number" className="form-control" id="inputAge"/>
+                <input type="number" className="form-control" min={18} max={100} id="inputAge" placeholder='Age'/>
             </div>
             <div className="mb-3">
-                <label for="inputPosition" className="form-label">Position</label>
-                <input type="text" className="form-control" id="inputPosition"/>
+                <input type="text" className="form-control" id="inputPosition" placeholder='Position'/>
             </div>
             <div className="mb-3">
-                <label for="inputPhone" className="form-label">Phone</label>
-                <input type="text" className="form-control" id="inputPhone"/>
+                <input type="number" className="form-control" id="inputPhone" max={9999999999} placeholder='Phone number'/>
             </div>
             <div className="mb-3 form-check">
                 <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                 <label className="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary" id='addButton'>Add</button>
         </form>
+        </>
     )
 }
 
