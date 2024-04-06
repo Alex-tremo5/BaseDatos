972f8495a6179319bb55513b83850e78f56d0e76
paulawon <166189769+paulawon@users.noreply.github.com>
2024-04-05 22:58:31 -0300

    Update Form Component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,46 +1,105 @@
 import React, { useState } from 'react'
 import CollaboratorsList from '../bd/collaborators.json'
 
-function FormCollaborator(props) {
+function FormCollaborator({ addNewCollaborator, totalCollaborators }) {
+    console.log(totalCollaborators)
 
-    const [nameCollab, setNameCollab] = useState ("")
-    const [collabList, setCollabList] = useState ([CollaboratorsList])
+    const [nombre, setName] = useState("")
+    const [correo, setEmail] = useState("@colaborador.com")
+    const [edad, setAge] = useState("")
+    const [cargo, setPosition] = useState("")
+    const [telefono, setPhone] = useState("569")
 
     const sendForm = (e) => {
         e.preventDefault()
-        setCollabList([...collabList, nameCollab])
-        setNameCollab("")
+        const newCollaborator = {
+            id: (totalCollaborators + 1).toString(),
+            nombre,
+            correo,
+            edad,
+            cargo,
+            telefono
+        };
+        addNewCollaborator(newCollaborator)
+        setName('');
+        setEmail('');
+        setAge('');
+        setPosition('');
+        setPhone('');
     }
-
-    const capturarInput = (e) => {
-        setNameCollab(e.target.value)
- }
 
     return (
         <>
-        <form className='registro' onSubmit = {sendForm}>
-            <h2>Add collaborator</h2>
-            <div className="mb-3">
-                <input type="text" className="form-control" id="inputName" placeholder='Name' onChange = {capturarInput}/>
-            </div>
-            <div className="mb-3">
-                <input type="email" className="form-control" id="inputEmail" placeholder='Email'/>
-            </div>
-            <div className="mb-3">
-                <input type="number" className="form-control" min={18} max={100} id="inputAge" placeholder='Age'/>
-            </div>
-            <div className="mb-3">
-                <input type="text" className="form-control" id="inputPosition" placeholder='Position'/>
-            </div>
-            <div className="mb-3">
-                <input type="number" className="form-control" id="inputPhone" max={9999999999} placeholder='Phone number'/>
-            </div>
-            <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" className="btn btn-primary" id='addButton'>Add</button>
-        </form>
+            <form className='registro' onSubmit={sendForm}>
+                <h2>Add collaborator</h2>
+                <div className="mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="inputName"
+                        value={nombre}
+                        placeholder='Name'
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="inputEmail"
+                        value={correo}
+                        placeholder='Email'
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <input
+                        type="number"
+                        className="form-control"
+                        min={18}
+                        max={100}
+                        id="inputAge"
+                        value={edad}
+                        placeholder='Age'
+                        onChange={(e) => setAge(e.target.value)}
+                        />
+                </div>
+                <div className="mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="inputPosition"
+                        value={cargo}
+                        placeholder='Position' 
+                        onChange={(e) => setPosition(e.target.value)}
+                        />
+                </div>
+                <div className="mb-3">
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="inputPhone"
+                        value={telefono}
+                        max={9999999999}
+                        placeholder='Phone number' 
+                        onChange={(e) => setPhone(e.target.value)}
+                        />
+                </div>
+                <div className="mb-3 form-check">
+                    <input
+                        type="checkbox"
+                        className="form-check-input"
+                        id="exampleCheck1"
+                    />
+                    <label
+                        className="form-check-label"
+                        for="exampleCheck1">Check me out</label>
+                </div>
+                <button
+                    type="submit"
+                    className="btn btn-primary"
+                    id='addButton'>Add</button>
+            </form>
         </>
     )
 }
